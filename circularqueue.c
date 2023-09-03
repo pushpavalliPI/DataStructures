2e67ebfa0651a54417ace43ae47b82835dc2cfdb
Pushpa <114721876+pushpavalliPI@users.noreply.github.com>
2023-09-03 19:03:37 +0530

    Circular queue implementation

---
+++ circularqueue.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+void enqueue(int x);
+int dequeue();
+void display();
+int isempty();
+int isfull();
+int front=-1;int rear=-1;
+int queue[10];
+int main()
+{
+    int c,n,d,ch;
+    do
+    {
+        printf("\nEnter 1/2/3\n1.Enqueue\n2.Dequeue\n3.Display\n");
+        scanf("%d",&c);
+        switch(c)
+        {
+            case 1:
+            printf("\nEnter element to be enqueued\n");
+            scanf("%d",&n);
+            enqueue(n);
+            break;
+            case 2:
+            d=dequeue();
+            printf("\n%d was dequeued\n",d);
+            break;
+            case 3:
+            display();
+            break;
+            default:
+            printf("\nPlease enter a valid choice\n");
+            break;
+        }
+        printf("\nDo you wish to continue(0/1)?\n");
+        scanf("%d",&ch);
+    } while (ch==1);
+    return 0;
+}
+void enqueue(int x)
+{
+    if (isfull())
+    {
+        printf("\nQueue is full!Cannot enqueue\n");
+    }
+    else
+    {
+        if (front==-1&&rear==-1)
+        {
+            front++;
+            rear++;
+            queue[rear]=x;
+        }
+        else
+        {
+            rear=(rear+1)%(10);
+            queue[rear]=x;
+        }
+    }
+}
+int dequeue()
+{
+    int y;
+    if(isempty())
+    {
+        printf("\nQueue is empty!Cannot dequeue!\n");
+    }
+    else
+    {
+        if(front==rear)
+        {
+            y=queue[front];
+            front=-1;
+            rear=-1;
+        }
+        else
+        {
+            y=queue[front];
+            front=(front+1)%(10);
+        }
+    }
+    return y;
+}
+void display()
+{
+    
+    if(isempty())
+    {
+        printf("\nQueue is empty!\n");
+    }
+    else
+    {
+        printf("\n Circular queue:\n");
+        for(int i=front;i!=rear;i=(i+1)%(10))
+        {
+            printf("  %d  ",queue[i]);
+        }
+    }
+}
+int isfull()
+{
+    if(rear == 9 && front == 0)
+    {
+        return 1;
+    }
+    else if(front == rear+1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int isempty()
+{
+    if(front == -1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+    
+}
